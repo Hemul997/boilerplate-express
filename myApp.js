eeeb3123c2ae2e3c37ba1b9add5eb46812dffca1
Hemul997 <nikitamoshkin1997@gmail.com>
2022-01-07 18:49:19 +0300

    Another commit

--- myApp.js
+++ myApp.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var app = express();
 
-//console.log("Hello World")
 
 app.get('/', function(req, res) {
-    //res.send('Hello Express');
     indexFileName = '/views/index.html';
     resourcePath = __dirname + indexFileName;
-    console.log(resourcePath);
     res.sendFile(resourcePath)
 });
 
