72a10b6a1eb7d39f5bb2527da656daa8bcee9001
Hemul997 <nikitamoshkin1997@gmail.com>
2022-01-07 18:48:25 +0300

    Add serving the file views/index.html to app

--- myApp.js
+++ myApp.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 
-console.log("Hello World")
+//console.log("Hello World")
 
 app.get('/', function(req, res) {
-    res.send('Hello Express')
+    //res.send('Hello Express');
+    indexFileName = '/views/index.html';
+    resourcePath = __dirname + indexFileName;
+    console.log(resourcePath);
+    res.sendFile(resourcePath)
 });
 
 
