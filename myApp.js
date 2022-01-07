2c4ea5afa55a4106632f6159e9647ab51902f20c
Hemul997 <nikitamoshkin1997@gmail.com>
2022-01-07 19:44:34 +0300

    Add using midleware in app

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 
 
+app.use('/public', express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
     indexFileName = '/views/index.html';
     resourcePath = __dirname + indexFileName;
