ba2a7e6cfb251e9587cf55430b58191c82034583
Hemul997 <nikitamoshkin1997@gmail.com>
2022-01-09 23:11:21 +0300

    Complete chalenge on "Basic Node and Express"

--- myApp.js
+++ myApp.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
-require('dotenv').config()
+require('dotenv').config();
 
 
 app.use('/public', express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use(function(req, res, next) {
+    console.log(req.method + ' ' + req.path + ' - ' + req.ip)
+    next();
+});
 
 app.get('/', function(req, res) {
     indexFileName = '/views/index.html';
@@ -11,11 +19,24 @@ app.get('/', function(req, res) {
     res.sendFile(resourcePath)
 });
 
+app.get('/:word/echo', function(req, res){
+    res.json({
+      "echo": req.params.word
+    }
+    );
+});
+
+app.get('/name', function(req, res) {
+    console.log(req.query)
+    res.json({
+        "name": req.query["first"] + ' ' + req.query["last"]
+    });
+});
+
 app.get('/json', function(req, res) {
     messageVal = "Hello json";
 
     if (process.env.MESSAGE_STYLE == "uppercase") {
-        console.log("Here")
         messageVal = messageVal.toUpperCase()
     }
 
@@ -23,22 +44,26 @@ app.get('/json', function(req, res) {
         "message": messageVal
     };
     res.json(jsonValue)
+
 });
 
+app.get('/now', function(req, res, next){
+    req.time = new Date().toString();
+    //res.send(req.time);
+    //console.log(req.time);
+    next();   
+}, function(req, res){
+    jsonValue = {
+        time: req.time
+    };
+    res.send(jsonValue)
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.post("/name", function(req, res) {
+    res.json({
+        "name": req.body["first"] + ' ' + req.body["last"]
+    });
+});
 
 
 
